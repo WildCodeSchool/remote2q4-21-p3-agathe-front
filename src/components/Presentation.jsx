9b0313615bc28bcdd66603e1a870991254e92ef1
Estelle <estelle9.dev@gmail.com>
2022-02-17 17:28:56 +0100

    code updated

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,21 +1,33 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import imageTest from "../assets/IMG_7178.jpeg";
 import "./Presentation.css";
 
 const Presentation = () => {
-  const [presentation, setPresentation] = useState('');
+  const [presentation, setPresentation] = useState("");
 
   useEffect(() => {
     const fetchPresentation = () => {
-      axios
-      .get(
-        `http://localhost:8000/api/presentation`
-      ).then((response)  => {setPresentation(response.data.presentation);});
+      axios.get(`http://localhost:8000/api/presentation`).then((response) => {
+        setPresentation(response.data.presentation.replace("\n"));
+      });
     };
     fetchPresentation();
   }, []);
 
-  return <div className="presentation">{presentation}</div>;
+  return (
+    <div className="presentation">
+      <div>
+        {presentation.split("\n").map((line) => (
+          <p>{line}</p>
+        ))}
+      </div>
+
+      <div className="presentation_img">
+        <img src={imageTest} alt="product" />
+      </div>
+    </div>
+  );
 };
 
 export default Presentation;
