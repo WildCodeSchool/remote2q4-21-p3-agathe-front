d8ab259c3b05adee5bf901ab8bd722a798cb7b12
YacineHATEM <94261237+YacineHATEM@users.noreply.github.com>
2022-02-23 15:49:42 +0100

    Update Marque.jsx

--- src/pages/Marque.jsx
+++ src/pages/Marque.jsx
@@ -22,16 +22,7 @@ function Marque() {
 
         <div className="Description">
             <Navbar />
-            <div>
-                <h1>Notre histoire</h1>
-                <p className="presentation"> Elfenn a été imaginée en Bretagne sud, à la croisée des elements naturesls.<br />
-                    Elle est née de l'intérêt de Pierre pour les huiles végétales et de la volonté de sa fille , <br />
-                    Agathe, de vous faire d'écouvrir la caméline et ses innombrables vertus au travers de soins adaptés pour tous.
-                    <br /><br />
-                    Nos huiles y sont fabriquées artisanalement pour sublimer tous leurs bienfaits Leurs compositions est à
-                    100%  d'origine naturelle, les ingrédients à 98% d' origine bretonne et flacons sont en verre, zero-dechet.
-                </p>
-            </div>
+         
             <div className="descrition_img">
                 <img className="img" src="/assets/img/logo.png" alt="logo" />
             </div>
