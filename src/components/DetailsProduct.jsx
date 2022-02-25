ec165fadd74b4ea55bd5fe116cbcb78ac33dc024
Salwa <salwa.touti@laposte.net>
2022-02-25 10:53:29 +0100

    css details

--- src/components/DetailsProduct.jsx
+++ src/components/DetailsProduct.jsx
@@ -4,15 +4,19 @@ import "./DetailsProduct.css";
 function DetailsProduct(props) {
 
     return (
-            <div className='DetailsProduct'>
-                <img className='DetailsProductImage' src={`/assets/img/${props.ProductID}.jpeg`} alt={props.name} />
-                <h4 className="DetailsProductTitle">{props.name}</h4>
-                <p className="DetailsProductPrice">{props.price} €</p>
-                <p className='DetailsProductCharacteristic'>{props.characteristic}</p>
-                <p className='DetailsProductDescription'> {props.description}</p>
-                <p className='DetailsProductIngredient'> {props.ingredient}</p>
+        <div className='DetailsProduct'>
+            <div className='DetailsContainer-top'>
+                < img className='DetailsProductImage' src={`/assets/img/${props.ProductID}.jpeg`} alt={props.name} />
+                <p className="DetailsProductTitle"> {props.name}</p>
             </div>
+            <div className='DetailsContainer-bottom'>
+            <p className="DetailsProductPrice">{props.price} €</p>
+            <p className='DetailsProductCharacteristic'>{props.characteristic}</p>
+            <p className='DetailsProductIngredient'> {props.ingredient}</p>
+            </div>
+            <p className='DetailsProductDescription'> {props.description}</p>
+        </div>
     )
 }
 
-export default DetailsProduct
+export default DetailsProduct;
