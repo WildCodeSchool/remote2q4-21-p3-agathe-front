25f74766daa7d98e798c7053eaede99b916515bf
Salwa <salwa.touti@laposte.net>
2022-02-25 12:05:35 +0100

    css details

--- src/components/DetailsProduct.jsx
+++ src/components/DetailsProduct.jsx
@@ -3,18 +3,30 @@ import "./DetailsProduct.css";
 
 function DetailsProduct(props) {
 
+    // <div className="presentation_text">
+    //     {presentation.split("\n").map((line) => (
+    //         <p>{line}</p>
+    //     ))}
+    // </div>
+
     return (
         <div className='DetailsProduct'>
-            <div className='DetailsContainer-top'>
+            <div className='DetailsContainer-left'>
                 < img className='DetailsProductImage' src={`/assets/img/${props.ProductID}.jpeg`} alt={props.name} />
+                <p className='DetailsProductDescription'> {props.description.split("\n").map((line) => (
+                    <p>{line}</p>
+                ))} </p>
+                <p className='DetailsProductCharacteristic'>{props.characteristic.split("\n").map((line) => (
+                    <p>{line}</p>
+                ))} </p>
+            </div>
+            <div className='DetailsContainer-right'>
                 <p className="DetailsProductTitle"> {props.name}</p>
+                <p className='DetailsProductIngredient'> {props.ingredient.split("\n").map((line) => (
+                    <p>{line}</p>
+                ))} </p>
+                <p className="DetailsProductPrice">{props.price} €</p>
             </div>
-            <div className='DetailsContainer-bottom'>
-            <p className="DetailsProductPrice">{props.price} €</p>
-            <p className='DetailsProductCharacteristic'>{props.characteristic}</p>
-            <p className='DetailsProductIngredient'> {props.ingredient}</p>
-            </div>
-            <p className='DetailsProductDescription'> {props.description}</p>
         </div>
     )
 }
