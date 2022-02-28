49978834b010a2b1d73c0c80972829e744cc507f
Stephen B <stephen-bailly_student2021@wilder.school>
2022-02-28 14:30:06 +0100

    First CSS

--- src/components/DetailsProduct.jsx
+++ src/components/DetailsProduct.jsx
@@ -3,24 +3,25 @@ import "./DetailsProduct.css";
 
 function DetailsProduct(props) {
     return (
+        <div className="DetailsContent">
+            <h2 className="DetailsTitle"> {props.name}</h2>
         <div className='DetailsProduct'>
-            <div className='DetailsContainer-left'>
-                <img className='DetailsProductImage' src={`/assets/img/${props.ProductID}.jpeg`} alt={props.name} />
-                <p className='DetailsProductDescription'>
+            <div className='DetailsImageContainer'>
+                <img className='DetailsImage' src={`/assets/img/${props.ProductID}.jpeg`} alt={props.name} />
+            </div>
+            <div className='DetailsText'>
+            <p className='DetailsDescription'>
                     {props.description && props.description.split("\n").map(line => <p>{line}</p>)}
                 </p>
-                <p className='DetailsProductCharacteristic'>
-                    {props.characteristic && props.characteristic.split("\n").map(line => <p>{line}</p>)}
-                </p>
+                <ul className='DetailsCharacteristic'>
+                    {props.characteristic && props.characteristic.split("\n").map(line => <li>{line}</li>)}
+                </ul>
+                <p className="DetailsPrice">{props.price} €</p>
+                <ul className='DetailsIngredient'>
+                    {props.ingredient && props.ingredient.split("\n").map(line => <li>{line}</li>)}
+                </ul>
             </div>
-            <div className='DetailsContainer-right'>
-                <p className="DetailsProductTitle"> {props.name}</p>
-                <p className='DetailsProductIngredient'>
-                    {props.ingredient && props.ingredient.split("\n").map(line => <p>{line}</p>)}
-                </p>
-                <p className="DetailsProductPrice">{props.price} €</p>
-            </div>
-        </div>
+        </div></div>
     )
 }
 
