fb6a1b84eb6548d71fc70a842c5ca4f8dea826d8
Stephen B <stephen-bailly_student2021@wilder.school>
2022-02-28 16:00:40 +0100

    Update CSS 2

--- src/components/DetailsProduct.jsx
+++ src/components/DetailsProduct.jsx
@@ -5,23 +5,23 @@ function DetailsProduct(props) {
     return (
         <div className="DetailsContent">
             <h2 className="DetailsTitle"> {props.name}</h2>
-        <div className='DetailsProduct'>
-            <div className='DetailsImageContainer'>
-                <img className='DetailsImage' src={`/assets/img/${props.ProductID}.jpeg`} alt={props.name} />
+            <div className='DetailsProduct'>
+                <div className='DetailsImageContainer'>
+                    <img className='DetailsImage' src={`/assets/img/${props.ProductID}.jpeg`} alt={props.name} />
+                </div>
+                <div className='DetailsText'>
+                    <p className='DetailsDescription'>
+                        {props.description && props.description.split("\n").map(line => <p>{line}</p>)}
+                    </p>
+                    <ul className='DetailsCharacteristic'>
+                        {props.characteristic && props.characteristic.split("\n").map(line => <li>{line}</li>)}
+                    </ul>
+                    <p className="DetailsPrice">{props.price} €</p>
+                    <ul className='DetailsIngredient'>
+                        {props.ingredient && props.ingredient.split("\n").map(line => <li>{line}</li>)}
+                    </ul>
+                </div>
             </div>
-            <div className='DetailsText'>
-            <p className='DetailsDescription'>
-                    {props.description && props.description.split("\n").map(line => <p>{line}</p>)}
-                </p>
-                <ul className='DetailsCharacteristic'>
-                    {props.characteristic && props.characteristic.split("\n").map(line => <li>{line}</li>)}
-                </ul>
-                <p className="DetailsPrice">{props.price} €</p>
-                <ul className='DetailsIngredient'>
-                    {props.ingredient && props.ingredient.split("\n").map(line => <li>{line}</li>)}
-                </ul>
-            </div>
-        </div>
         </div>
     )
 }
