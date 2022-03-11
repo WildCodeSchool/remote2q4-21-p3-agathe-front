556a844db7ef83cd0ed56ff10e60631f9427afa8
Stéphen <stephen-bailly_student2021@wilder.school>
2022-03-11 16:39:46 +0100

    Update LoginForm.jsx

--- src/components/form/LoginForm.jsx
+++ src/components/form/LoginForm.jsx
@@ -24,7 +24,7 @@ const RegisterForm = () => {
           setUser({
             token: credentials,
           });
-          redirect("/#homepage");
+          redirect(-1);
         });
     } catch (error) {
       console.log(error);
