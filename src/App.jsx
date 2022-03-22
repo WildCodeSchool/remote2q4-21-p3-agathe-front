849ca752ff3f0f0e1bfbab68717d0b106731bd7f
Sylvain Fauveau <sylvain-fauveau_student2021@wilder.school>
2022-03-22 16:02:49 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,6 @@ function App() {
               <Route path="/register" element={<RegisterPage />} />
               <Route path="/logout" element={<Logout />} />
               <Route path="/admin/presentation" element={<TextAdmin />} />
-              <Route path="/cart" element={<CartPage />} />
             </Routes>
           </BrowserRouter>
         </CartProvider>
