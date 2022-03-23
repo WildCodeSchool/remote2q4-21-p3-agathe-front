62ba29e516eaa4f0e891b17525105026d97df8d1
Salwa <salwa.touti@laposte.net>
2022-03-23 17:16:15 +0100

    change import

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
+import AdminPanel from "./pages/AdminPanel";
 import Brand from "./pages/Brand";
-import ProductPage from "./pages/ProductPage";
-import CartPage from "./pages/CartPage";
 import Catalogue from "./pages/Catalogue";
-import Checkout from "./pages/Checkout";
+import CartPage from "./pages/CartPage";
 import ContactPage from "./pages/ContactPage";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage"
-import Logout from "./components/form/Logout";
-import TextAdmin from "./pages/TextAdmin";
 import CartProvider from "./contexts/CartContext";
+import Checkout from "./pages/Checkout";
+import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import Logout from "./components/form/Logout";
+import Page from "./middleware/Page";
+import ProductPage from "./pages/ProductPage";
+import RegisterPage from "./pages/RegisterPage"
+import TextAdmin from "./pages/TextAdmin";
 import UserProvider from './contexts/UserProvider';
 import "./App.css"
-import AdminPanel from "./pages/AdminPanel";
-import Page from "./middleware/Page";
 
 function App() {
   return (
@@ -30,7 +30,7 @@ function App() {
               <Route path="/cart" element={<Page SubPage={CartPage} />} />
               <Route path="/catalogue" element={<Catalogue />} />
               <Route path="/checkout" element={<Checkout />} />
-              <Route path="/contact" element={<ContactPage />} />
+              <Route path="/contact" element={<Page SubPage={ContactPage} />} />
               <Route path="/login" element={<LoginPage />} />
               <Route path="/register" element={<RegisterPage />} />
               <Route path="/logout" element={<Logout />} />
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import ContactForm from "../components/form/ContactForm";
-import Footer from '../components/home/Footer';
-import Navbar from '../components/home/Navbar';
+
 import "./ContactPage.css";
+
 
 function ContactPage() {
   return (
-    <div className="ContactPage">
-      <Navbar />
-        <div className='FormContainer'>
-            <h1 className="contact">Nous contacter</h1>
-            <a className="email" href="mailto: elfenncosmetiques@gmail.com" target="_blank" rel="noreferrer">elfenncosmetiques@gmail.com</a>
-            <ContactForm  />
-        </div>
-        <Footer />
+    <div className='ContactPage'>
+      <h1 className="contact">Nous contacter</h1>
+      <a className="email" href="mailto: elfenncosmetiques@gmail.com" target="_blank" rel="noreferrer">elfenncosmetiques@gmail.com</a>
+      <ContactForm />
     </div>
   )
 }
