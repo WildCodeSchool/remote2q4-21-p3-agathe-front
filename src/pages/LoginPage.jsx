f25aef501440f221c8c1d6d2a9f3af40144165d3
mentat51 <sylvain.fauveau@gmail.com>
2022-03-24 12:12:46 +0100

    Page in login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,19 +1,8 @@
-import React from 'react';
-import Footer from '../components/home/Footer';
+import React from "react";
 import LoginForm from "../components/form/LoginForm";
-import Navbar from '../components/home/Navbar';
-import "./LoginPage.css";
 
 function LoginPage() {
-  return (
-    <div className="LoginPage">
-      <Navbar />
-      <div className='LoginContainer'>
-        <LoginForm />
-      </div>
-      <Footer />
-    </div>
-  )
+    return <LoginForm />;
 }
 
 export default LoginPage
