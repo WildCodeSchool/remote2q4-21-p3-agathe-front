b293e5441c0da639b2519d5c710636cc6411f893
mentat51 <sylvain.fauveau@gmail.com>
2022-03-28 11:29:30 +0200

    Adding orders detail to admin panel

--- src/components/admin/Table.jsx
+++ src/components/admin/Table.jsx
@@ -1,14 +1,26 @@
-import "./Table.css"
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import React from "react";
+import axios from "axios";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import "./Table.css";
+
+const BASE_URL = process.env.REACT_APP_URL_SERVER;
 
 const List = () => {
-    const rows = [
+    const [rows, setRows] = React.useState(null);
+
+    React.useEffect(() => {
+        axios
+            .get(`${BASE_URL}/api/orders`)
+            .then((response) => setRows(response.data));
+    }, []);
+
+    const rowsOld = [
         {
             id: 1143155,
             product: "",
@@ -19,46 +31,6 @@ const List = () => {
             method: "Cash on Delivery",
             status: "Approved",
         },
-        {
-            id: 2235235,
-            product: "",
-            img: "",
-            customer: "Michael Doe",
-            date: "1 March",
-            amount: 900,
-            method: "Online Payment",
-            status: "Pending",
-        },
-        {
-            id: 2342353,
-            product: "",
-            img: "",
-            customer: "John Smith",
-            date: "1 March",
-            amount: 35,
-            method: "Cash on Delivery",
-            status: "Pending",
-        },
-        {
-            id: 2357741,
-            product: "",
-            img: "",
-            customer: "Jane Smith",
-            date: "1 March",
-            amount: 920,
-            method: "Online",
-            status: "Approved",
-        },
-        {
-            id: 2342355,
-            product: "",
-            img: "",
-            customer: "Harold Carol",
-            date: "1 March",
-            amount: 2000,
-            method: "Online",
-            status: "Pending",
-        },
     ];
 
     return (
@@ -66,38 +38,57 @@ const List = () => {
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
                 <TableHead>
                     <TableRow>
-                        <TableCell className="tableCell">Tracking ID</TableCell>
-                        <TableCell className="tableCell">Product</TableCell>
-                        <TableCell className="tableCell">Customer</TableCell>
+                        <TableCell className="tableCell">Commande</TableCell>
+                        <TableCell className="tableCell">Produit</TableCell>
+                        <TableCell className="tableCell">Client</TableCell>
                         <TableCell className="tableCell">Date</TableCell>
-                        <TableCell className="tableCell">Amount</TableCell>
-                        <TableCell className="tableCell">Payment Method</TableCell>
+                        <TableCell className="tableCell">Montant</TableCell>
+                        <TableCell className="tableCell">
+                            Payment Method
+                        </TableCell>
                         <TableCell className="tableCell">Status</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {rows.map((row) => (
-                        <TableRow
-                            key={row.id}>
-                            <TableCell className="tableCell">{row.id}</TableCell>
-                            <TableCell className="tableCell">
-                                <div className="cellWrapper">
-                                    <img src={row.img} alt="" className="datatableImage" />
-                                    {row.product}
-                                </div>
-                            </TableCell>
-                            <TableCell className="tableCell">{row.customer}</TableCell>
-                            <TableCell className="tableCell">{row.date}</TableCell>
-                            <TableCell className="tableCell">{row.amount}</TableCell>
-                            <TableCell className="tableCell">{row.method}</TableCell>
-                            <TableCell className="tableCell"><span className={`status ${row.status}`}>{row.status}</span></TableCell>
-                        </TableRow>
-                    ))}
+                    {rows &&
+                        rows.map((row) => (
+                            <TableRow key={row.OrderId}>
+                                <TableCell className="tableCell">
+                                    {row.OrderId}
+                                </TableCell>
+                                <TableCell className="tableCell">
+                                    <div className="cellWrapper">
+                                        <img
+                                            src={row.img}
+                                            alt=""
+                                            className="datatableImage"
+                                        />
+                                        {row.product}
+                                    </div>
+                                </TableCell>
+                                <TableCell className="tableCell">
+                                    {row.name}
+                                </TableCell>
+                                <TableCell className="tableCell">
+                                    {new Date(row.OrderDate).toLocaleDateString()}
+                                </TableCell>
+                                <TableCell className="tableCell">
+                                    {row.amount}
+                                </TableCell>
+                                <TableCell className="tableCell">
+                                    {row.method}
+                                </TableCell>
+                                <TableCell className="tableCell">
+                                    <span className={`status ${row.status}`}>
+                                        {row.status}
+                                    </span>
+                                </TableCell>
+                            </TableRow>
+                        ))}
                 </TableBody>
             </Table>
         </TableContainer>
     );
-}
+};
 
-
-export default List
+export default List;
