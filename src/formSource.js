288c0d86c42ae177296e068324ddee4740a99cdb
Salwa <salwa.touti@laposte.net>
2022-03-29 16:02:53 +0200

    merge

--- src/formSource.js
+++ src/formSource.js
@@ -1,76 +1,95 @@
 export const userInputs = [
-    {
-      id: 1,
-      label: "Username",
-      type: "text",
-      placeholder: "john_doe",
-    },
-    {
-      id: 2,
-      label: "Name and surname",
-      type: "text",
-      placeholder: "John Doe",
-    },
-    {
-      id: 3,
-      label: "Email",
-      type: "mail",
-      placeholder: "john_doe@gmail.com",
-    },
-    {
-      id: 4,
-      label: "Phone",
-      type: "text",
-      placeholder: "+1 234 567 89",
-    },
-    {
-      id: 5,
-      label: "Password",
-      type: "password",
-    },
-    {
-      id: 6,
-      label: "Address",
-      type: "text",
-      placeholder: "Elton St. 216 NewYork",
-    },
-    {
-      id: 7,
-      label: "Country",
-      type: "text",
-      placeholder: "USA",
-    },
-  ];
-  
-  export const productInputs = [
-    {
-      id: 1,
-      label: "Title",
-      type: "text",
-      placeholder: "Apple Macbook Pro",
-    },
-    {
-      id: 2,
-      label: "Description",
-      type: "text",
-      placeholder: "Description",
-    },
-    {
-      id: 3,
-      label: "Category",
-      type: "text",
-      placeholder: "Computers",
-    },
-    {
-      id: 4,
-      label: "Price",
-      type: "text",
-      placeholder: "100",
-    },
-    {
-      id: 5,
-      label: "Stock",
-      type: "text",
-      placeholder: "in stock",
-    },
-  ];
+  {
+    id: 1,
+    label: "FirstName",
+    type: "text",
+    placeholder: "Louis",
+  },
+  {
+    id: 2,
+    label: "LastName",
+    type: "text",
+    placeholder: "Dubois",
+  },
+  {
+    id: 3,
+    label: "Email",
+    type: "mail",
+    placeholder: "louis_dubois@gmail.com",
+  },
+  {
+    id: 4,
+    label: "Phone",
+    type: "text",
+    placeholder: "0123456789",
+  },
+  {
+    id: 5,
+    label: "Password",
+    type: "password",
+  },
+  {
+    id: 6,
+    label: "Address1",
+    type: "text",
+    placeholder: "Av. des Champs-Élysées",
+  },
+
+  {
+    id: 7,
+    label: "Address2",
+    type: "text",
+    placeholder: "",
+  },
+  {
+    id: 8,
+    label: "Address3",
+    type: "text",
+    placeholder: "",
+  },
+  {
+    id: 9,
+    label: "postCode",
+    type: "text",
+    placeholder: "75008 ",
+  },
+  {
+    id: 10,
+    label: "city",
+    type: "text",
+    placeholder: "Paris",
+  },
+];
+
+export const productInputs = [
+  {
+    id: 1,
+    label: "Title",
+    type: "text",
+    placeholder: "Apple Macbook Pro",
+  },
+  {
+    id: 2,
+    label: "Description",
+    type: "text",
+    placeholder: "Description",
+  },
+  {
+    id: 3,
+    label: "Category",
+    type: "text",
+    placeholder: "Computers",
+  },
+  {
+    id: 4,
+    label: "Price",
+    type: "text",
+    placeholder: "100",
+  },
+  {
+    id: 5,
+    label: "Stock",
+    type: "text",
+    placeholder: "in stock",
+  },
+];
