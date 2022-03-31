b17c1311bf0f04e9d0c9cd5b6f6d86292dc7790c
mentat51 <sylvain.fauveau@gmail.com>
2022-03-31 10:42:38 +0200

    a tester

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { useCart } from "../contexts/CartContext";
 import { useUser } from '../contexts/UserProvider';
