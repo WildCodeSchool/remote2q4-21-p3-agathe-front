81a54b7c310afdc0f8ce3b07ac70c32112573148
Stephen Bailly <stephen.bailly@gmail.com>
2022-04-06 15:51:53 +0200

    BG added ContactPage No Resp

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import ContactForm from "../components/form/ContactForm";
-
 import "./ContactPage.css";
 
 function ContactPage() {
     return (
-        <>
-            <h1 className="contact">Nous contacter</h1>
-            <a
-                className="email"
-                href="mailto: elfenncosmetiques@gmail.com"
-                target="_blank"
-                rel="noreferrer"
-            >
-                elfenncosmetiques@gmail.com
-            </a>
-            <ContactForm />
-        </>
+        <div className="contactPage">
+            <div className="contactLeft">
+                <h1 className="contact">Nous contacter</h1>
+                <a
+                    className="email"
+                    href="mailto: elfenncosmetiques@gmail.com"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    elfenncosmetiques@gmail.com
+                </a>
+                <ContactForm />
+            </div>
+            <div className="contactRight">
+            </div>
+        </div >
     );
 }
 
