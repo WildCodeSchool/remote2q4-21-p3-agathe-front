f10854f01a64cb2806ce77b07af371d3186a1fae
Estelle <estelle9.dev@gmail.com>
2022-04-08 15:53:21 +0200

    css fixed

--- src/components/cart/CartTotal.jsx
+++ src/components/cart/CartTotal.jsx
@@ -10,14 +10,6 @@ const CartTotal = () => {
       <h2>Total de la commande</h2>
       <p>Frais de port</p>
       <p className="cmd-total"><b>Total :</b> {cart.total.toFixed(2)} €</p>
-      {/* <div className="payment-container">
-        <p>Paiements acceptés :</p>
-        <div className="cards-icons">
-        <FaIcons.FaCcVisa className="payment-cards" />
-        <FaIcons.FaCcMastercard className="payment-cards" />
-        <FaIcons.FaCcPaypal className="payment-cards" />
-        </div>
-      </div> */}
       <Link to="/checkout" className="btn">
         <button>Passer la commande</button>
       </Link>
