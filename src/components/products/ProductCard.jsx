8bf86d49a35c8f5cc925e8cbf78632ce0196e4b1
Estelle <estelle9.dev@gmail.com>
2022-04-08 15:37:26 +0200

    responsive card link done

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+// import { HiPlusCircle } from "react-icons/hi";
 import { Link } from "react-router-dom";
+import { MdInfoOutline, MdOutlineAddShoppingCart } from "react-icons/md";
 import { useCart } from "../../contexts/CartContext";
 import Modal from "../modal/modal"
 import useModal from "../modal/useModal";
@@ -11,11 +13,26 @@ const BASE_URL = process.env.REACT_APP_URL_SERVER;
 function ProductCard(props) {
   const cart = useCart();
   const { isShowing: isModalCartShowed, toggle: toggleModalCart } = useModal();
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  
   const handleAddToCart = () => {
     const product = { ...props, quantity: 1 };
     cart.add(product);
     toggleModalCart();
   };
+
+  useEffect(() => {
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", changeWidth);
+
+    return () => {
+      window.removeEventListener("resize", changeWidth);
+    };
+  }, []);
+
   return (
     <div className={`ProductCardBase ${props.className}`}>
       <img
@@ -26,12 +43,17 @@ function ProductCard(props) {
       <h4 className="ProductCardTitle">{props.name}</h4>
       <p className="ProductCardPrice">{props.price} €</p>
       <div className="ProductCardBase_buttons">
-        <Link to={`/page_produit/${props.id}`} className="link--hover">
+        {screenWidth > 500 ? <><Link to={`/page_produit/${props.id}`} className="link--hover">
           <button>Détail</button>
         </Link>
         <Link to="#" className="link--hover">
           <button onClick={handleAddToCart}>Panier</button>
+        </Link></> : <><Link to={`/page_produit/${props.id}`} className="link--icons">
+          <MdInfoOutline className="card-icon"/>
         </Link>
+        <Link to="#" className="link--icons">
+          <MdOutlineAddShoppingCart className="card-icon" onClick={handleAddToCart}/>
+        </Link></>}
       </div>
       <Modal isShowing={isModalCartShowed} hide={toggleModalCart} title="Que souhaitez vous faire ?">
         <ModalCart toggle={toggleModalCart}/>
